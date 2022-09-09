99b189c37c3101ed76bf04b5864ab1b078bef712
Sean Keane <seankeane93@gmail.com>
2022-09-09 16:04:24 -0700

    Fixed errors, added migrations, and updated database

--- Startup.cs
+++ Startup.cs
@@ -1,18 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using AnimalShelter.Models;
 
-namespace AnimalShelterAPI.Solution
+namespace AnimalShelter
 {
     public class Startup
     {
@@ -27,11 +21,9 @@ namespace AnimalShelterAPI.Solution
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddDbContext<AnimalShelterContext>(opt =>
+                opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AnimalShelterAPI.Solution", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +32,6 @@ namespace AnimalShelterAPI.Solution
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AnimalShelterAPI.Solution v1"));
             }
 
             // app.UseHttpsRedirection();
